c323822d71c4dd70aa39b8789f60ba78dc2eedee
Shenno Willaert <shenno.w@gmail.com>
2016-11-03 16:12:40 +0100

    backingbean

--- src/main/java/com/realdolmen/scrumex/beans/BackingBean.java
+++ src/main/java/com/realdolmen/scrumex/beans/BackingBean.java
@@ -1,12 +1,14 @@
 package com.realdolmen.scrumex.beans;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import com.realdolmen.scrumex.domain.Question;
 import com.realdolmen.scrumex.services.QuestionsRepositoryInterface;
 
 @Named
@@ -16,9 +18,27 @@ public class BackingBean implements Serializable {
 	@Inject
 	QuestionsRepositoryInterface repo;
 
+	List<Question> questions;
+
 	@PostConstruct
 	private void init() {
+		questions = repo.getAllQuestions();
+	}
 
+	public QuestionsRepositoryInterface getRepo() {
+		return repo;
+	}
+
+	public void setRepo(QuestionsRepositoryInterface repo) {
+		this.repo = repo;
+	}
+
+	public List<Question> getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(List<Question> questions) {
+		this.questions = questions;
 	}
 
 }
