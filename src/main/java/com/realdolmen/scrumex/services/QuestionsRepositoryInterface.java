8d7bcdc1cbb7e003299959c17962143728b0f25d
Frederik Van Herbruggen <frederik.vanh@gmail.com>
2016-11-03 15:08:02 +0100

    added interface for repo

---
+++ src/main/java/com/realdolmen/scrumex/services/QuestionsRepositoryInterface.java
@@ -0,0 +1,10 @@
+package com.realdolmen.scrumex.services;
+
+import java.util.List;
+
+/**
+ * Created by Frederik Van Herbruggen on 3/11/2016.
+ */
+public interface QuestionsRepositoryInterface {
+    public List<Object> getAllQuestions();
+}
